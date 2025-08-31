a89780ef0715ff9b67cfdc6db67300b12075f223
sumir <sumir40@gmail.com>
2025-09-01 00:54:36 +0530

    fix .evn

--- config.py
+++ config.py
@@ -2,14 +2,23 @@ import os
 from dotenv import load_dotenv
 import logging
 
-env_path = './ENV/.env'
-load_dotenv(dotenv_path=env_path)
 
-# load_dotenv()
+
+load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OpenAI')
+
+logging.info(f"[INFO] OpenAi  - {OPENAI_API_KEY}")
+
+env_path = './ENV/.env'
+
+
+load_dotenv(dotenv_path=env_path)
+
+
 MOZ_API_KEY = os.getenv('MozApi')
 SAERCH_API_KEY = os.getenv('TavilyApi')
 
 
 logging.info(f"[INFO] MOZ API KEY - {MOZ_API_KEY}")
+

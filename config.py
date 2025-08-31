bf13cc41fa7f936ec38990273a1df002f19e4c77
sumir <sumir40@gmail.com>
2025-09-01 01:39:27 +0530

    add .evn file 12

--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 import logging
 
-
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv('OpenAI')
-
-logging.info(f"[INFO] OpenAi  - {OPENAI_API_KEY}")
-
 env_path = './ENV/.env'
-
-
 load_dotenv(dotenv_path=env_path)
 
+
+OPENAI_API_KEY = os.getenv('OpenAI')
+logging.info(f"[INFO] OpenAi  - {OPENAI_API_KEY}")
 
 MOZ_API_KEY = os.getenv('MozApi')
 SAERCH_API_KEY = os.getenv('TavilyApi')
 
 
 logging.info(f"[INFO] MOZ API KEY - {MOZ_API_KEY}")
+
+config = dotenv_values()
+
+for key, value in config.items():
+    logging.info(f"[secret] - {key}: {value}")
+
 
